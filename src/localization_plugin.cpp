e67a3d058fe4bc99820738f5d7647dc07ee9ad7c
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-10-21 14:46:50 +0000

    Inflation works
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/trunk/ed_localization@27975 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- src/localization_plugin.cpp
+++ src/localization_plugin.cpp
@@ -37,11 +37,12 @@ class CellData
 {
 public:
 
-  CellData(int index_, double distance_) :
-      index(index_), distance(distance_)
+  CellData(int index_, int mx_, int my_, int mx_src_, int my_src_, double distance_) :
+      index(index_), mx(mx_), my(my_), mx_src(mx_src_), my_src(my_src_), distance(distance_)
   {}
 
   int index;
+  int mx, my, mx_src, my_src;
   double distance;
 
 };
@@ -59,11 +60,12 @@ class KernelCell
 {
 public:
 
-    KernelCell(int d_index_, double d_dist_) :
-        d_index(d_index_), ddistance(d_dist_)
+    KernelCell(int d_index_, int dx_, int dy_, double d_dist_) :
+        d_index(d_index_), dx(dx_), dy(dy_), ddistance(d_dist_)
     {}
 
     int d_index;
+    int dx, dy;
     double ddistance;
 };
 
@@ -153,12 +155,17 @@ void LocalizationPlugin::process(const ed::WorldModel& world, ed::UpdateRequest&
     tue::Timer timer;
     timer.start();
 
-    cv::Mat distance_map(800, 800, CV_32FC1, max_dist);
+    double res = 0.025;
+
+    cv::Mat distance_map(800, 800, CV_32FC1, (max_dist / res) * (max_dist / res));
 
     std::vector<geo::Vector3> points;
     lrf_.rangesToPoints(ranges_out, points);
 
-    double res = 0.025;
+
+//    points.clear();
+//    points.push_back(geo::Vector3(-0.1, 0, 0));
+//    points.push_back(geo::Vector3(0.1, 0, 0));
 
     std::priority_queue<CellData> Q;
     for(unsigned int i = 0; i < points.size(); ++i)
@@ -169,50 +176,68 @@ void LocalizationPlugin::process(const ed::WorldModel& world, ed::UpdateRequest&
 
         if (x >= 0 && y >= 0 && x < distance_map.cols && y < distance_map.rows) {
             int index = y * distance_map.cols + x;
-            distance_map.at<float>(index) = 0;
-            Q.push(CellData(index, 0));
+            Q.push(CellData(index, x, y, x, y, 0));
         }
     }
 
     std::vector<KernelCell> kernel;
 
-    kernel.push_back(KernelCell(-distance_map.cols, res));
-    kernel.push_back(KernelCell(distance_map.cols, res));
-    kernel.push_back(KernelCell(-1, res));
-    kernel.push_back(KernelCell( 1, res));
+    kernel.push_back(KernelCell(-distance_map.cols, 0, -1, res));
+    kernel.push_back(KernelCell(distance_map.cols, 0, 1, res));
+    kernel.push_back(KernelCell(-1, -1, 0, res));
+    kernel.push_back(KernelCell( 1,  1, 0, res));
 
-    kernel.push_back(KernelCell(-1 - distance_map.cols, sqrt(2 * res * res)));
-    kernel.push_back(KernelCell( 1 - distance_map.cols, sqrt(2 * res * res)));
-    kernel.push_back(KernelCell(-1 + distance_map.cols, sqrt(2 * res * res)));
-    kernel.push_back(KernelCell(-1 + distance_map.cols, sqrt(2 * res * res)));
+//    kernel.push_back(KernelCell(-1 - distance_map.cols, sqrt(2.0 * res * res)));
+//    kernel.push_back(KernelCell( 1 - distance_map.cols, sqrt(2.0 * res * res)));
+//    kernel.push_back(KernelCell(-1 + distance_map.cols, sqrt(2.0 * res * res)));
+//    kernel.push_back(KernelCell( 1 + distance_map.cols, sqrt(2.0 * res * res)));
 
     while(!Q.empty())
     {
         const CellData& c = Q.top();
+        int mx = c.mx;
+        int my = c.my;
+        int mx_src = c.mx_src;
+        int my_src = c.my_src;
         int index = c.index;
         double distance = c.distance;
         Q.pop();
 
-        for(unsigned int i = 0; i < kernel.size(); ++i)
+        double current_distance = distance_map.at<float>(index);
+        if (distance < current_distance)
         {
-            const KernelCell& kc = kernel[i];
-
-            int new_index = index + kc.d_index;
-            double old_distance = distance_map.at<float>(new_index);
-            double new_distance = distance + kc.ddistance;
-
-            if (new_distance < old_distance)
+            distance_map.at<float>(index) = distance;
+            for(unsigned int i = 0; i < kernel.size(); ++i)
             {
-                distance_map.at<float>(new_index) = new_distance;
-                Q.push(CellData(new_index, new_distance));
+                const KernelCell& kc = kernel[i];
+                int new_index = index + kc.d_index;
+                int mx_new = mx + kc.dx;
+                int my_new = my + kc.dy;
+
+                int dx = mx_new - mx_src;
+                int dy = my_new - my_src;
+
+//                double new_distance = distance + kc.ddistance;
+                double new_distance = (dx * dx) + (dy * dy);
+                Q.push(CellData(new_index, mx_new, my_new, mx_src, my_src, new_distance));
             }
         }
+
+//        std::cout << index << ": " << distance << std::endl;
+
     }
 
     std::cout << timer.getElapsedTimeInMilliSec() << " ms" << std::endl;
 
-    cv::imshow("distance_map", distance_map / max_dist);
-    cv::waitKey(3);
+    std::cout << distance_map.at<float>(distance_map.rows / 2, distance_map.cols / 2 + 4) << std::endl;
+
+
+    cv::imshow("distance_map", distance_map / ((max_dist / res) * (max_dist / res)));
+    cv::waitKey(1);
+
+
+
+
 }
 
 // ----------------------------------------------------------------------------------------------------
