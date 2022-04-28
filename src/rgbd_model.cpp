41af5cfb3eafb749d3a7e24b5d3aacb10d6b8526
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-04-28 14:56:26 +0200

    (rgbd) print when unique samples are not the same size

--- src/rgbd_model.cpp
+++ src/rgbd_model.cpp
@@ -243,6 +243,10 @@ bool RGBDModel::updateWeights(const ed::WorldModel& world, std::future<const Mas
         ROS_ERROR_NAMED("rgbd_model", "(RGBD) Only one unique sample found");
         return false;
     }
+    if (unique_samples.size() != pf.samples().size())
+    {
+        ROS_ERROR_STREAM_NAMED("rgbd_model", "Unique_samples is not the same size: " << unique_samples.size() << " vs " << pf.samples().size());
+    }
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     // -     Render world model type/depth image
