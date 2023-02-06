73bae5bf2121c62f7a57293f3a54c5dc24c3e72b
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2023-02-06 10:50:36 +0100

    Write cluster data to yaml

--- tools/particles_analyzer.cpp
+++ tools/particles_analyzer.cpp
@@ -4,6 +4,7 @@
 
 #include <tue/config/configuration.h>
 #include <tue/config/loaders/yaml.h>
+#include <tue/config/write.h>
 #include <tue/filesystem/crawler.h>
 #include <tue/filesystem/path.h>
 
@@ -51,11 +52,51 @@ int main(int argc, char **argv)
     ed_localization::ParticleFilter pf;
     pf.configure(config);
 
+    tue::Configuration cluster_data;
+
     std::vector<fs::path> paths(fs::directory_iterator(fs::path(particle_dir.string())), fs::directory_iterator{});
     std::sort(paths.begin(), paths.end());
-    tue::filesystem::Path particle_path;
+    cluster_data.writeArray("files");
     for(const fs::path& path: paths)
     {
-        std::cout << "path: '" << path.string() << "'" << std::endl;
+        if (path.extension().string() != ".csv")
+            continue;
+        pf.loadCSV(path.string());
+        cluster_data.addArrayItem();
+        cluster_data.setValue("name", path.filename().string());
+        cluster_data.writeArray("clusters");
+        for (const ed_localization::Cluster& c : pf.clusters())
+        {
+            cluster_data.addArrayItem();
+            cluster_data.setValue("weight", c.weight);
+            cluster_data.writeGroup("mean");
+            cluster_data.setValue("x", c.mean.getOrigin().x);
+            cluster_data.setValue("y", c.mean.getOrigin().y);
+            cluster_data.setValue("theta", c.mean.rotation());
+            cluster_data.endGroup();
+            bool first_item = true;
+            std::stringstream ss;
+            ss << "[";
+            for (const double& v : c.cov.m)
+            {
+                if (!first_item)
+                    ss << ", ";
+                ss << v;
+                first_item = false;
+            }
+            ss << "]";
+            cluster_data.setValue("cov", ss.str());
+            cluster_data.endArrayItem();
+        }
+        cluster_data.endArray();
+        cluster_data.endArrayItem();
     }
+    cluster_data.endArray();
+    tue::filesystem::Path cluster_data_yaml = particle_dir.join("cluster_data.yaml");
+    if (!tue::config::toFile(cluster_data_yaml.string(), cluster_data.data(), tue::config::YAML, 4))
+    {
+        std::cerr << "Failed to write to '" << cluster_data_yaml.string() << "'" << std::endl;
+        return 1;
+    }
+    return 0;
 }
